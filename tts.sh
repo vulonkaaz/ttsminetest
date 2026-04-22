f2dbf0287bce2db29c21e2fc9311f379993b5a58
vulonkaaz <7442677+vulonkaaz@users.noreply.github.com>
2026-04-22 17:58:17 +0200

    fix color escaping

--- tts.sh
+++ tts.sh
@@ -3,6 +3,6 @@
 grep --line-buffered -F "[CHATTTS]"|grep --line-buffered -F "<"|while read line
 do
 	pitch=$(echo $line|awk -F"[<>]" '{print $2}'| sha256sum | grep -Eo "[[:digit:]]{2}"  | head -n1)
-	msg=$(echo $line|sed 's/@#[A-Za-z0-9]\{6\})//g;s/^[^>]*>//')
+	msg=$(echo $line|sed 's/^[^>]*>//')
 	echo $msg|espeak -p $pitch
 done
--- tts/init.lua
+++ tts/init.lua
@@ -1,4 +1,4 @@
 core.register_on_receiving_chat_message(function(message)
-	core.log("action", "[CHATTTS] " .. message)
+	core.log("action", "[CHATTTS] " .. core.strip_colors(message))
 	return false
 end)
