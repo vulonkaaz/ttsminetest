2a8acb24d9da318ebe9aeebc64538ec88578900a
vulonkaaz <7442677+vulonkaaz@users.noreply.github.com>
2026-04-22 18:20:19 +0200

    fix a bug i don't quite understand

--- tts.sh
+++ tts.sh
@@ -2,7 +2,7 @@
 
 grep --line-buffered -F "[CHATTTS]"|grep --line-buffered -F "<"|while read line
 do
-	pitch=$(echo $line|awk -F"[<>]" '{print $2}'| sha256sum | grep -Eo "[[:digit:]]{2}"  | head -n1)
-	msg=$(echo $line|sed 's/^[^>]*>//')
-	echo $msg|espeak -p $pitch
+	pitch=$(echo "$line"|awk -F"[<>]" '{print $2}'| sha256sum | grep -Eo "[[:digit:]]{2}"  | head -n1)
+	msg=$(echo "$line"|sed 's/^[^>]*>//')
+	echo "$msg"|espeak -p $pitch
 done
