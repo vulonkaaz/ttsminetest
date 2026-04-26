41f5f5529381664cb6f4827f2a8dee5b63c2af50
vulonkaaz <7442677+vulonkaaz@users.noreply.github.com>
2026-04-26 23:13:04 +0200

    add TTS toggle

--- tts/init.lua
+++ tts/init.lua
@@ -1,4 +1,22 @@
+local modstorage = core.get_mod_storage()
+
 core.register_on_receiving_chat_message(function(message)
-	core.log("action", "[CHATTTS] " .. core.strip_colors(message))
+	if modstorage:get_int('tts_enabled') == 1 then
+		core.log("action", "[CHATTTS] " .. core.strip_colors(message))
+	end
 	return false
 end)
+
+core.register_chatcommand('tts', {
+	params = '',
+	description = 'toggles text to speech',
+	func = function()
+		if modstorage:get_int('tts_enabled') == 1 then
+			modstorage:set_int('tts_enabled', 0)
+			core.display_chat_message('TTS disabled')
+		else
+			modstorage:set_int('tts_enabled', 1)
+			core.display_chat_message('TTS enabled')
+		end
+	end,
+})
